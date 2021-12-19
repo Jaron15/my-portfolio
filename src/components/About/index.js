9019318d4364cf5b8ef8290f4e99e8e6e1a461ad
Jaron <jjkenyon777@gmail.com>
2021-12-18 19:57:46 -0700

    update about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,9 @@ function About() {
       <h1 id="about">Who am I?</h1>
       <div className="my-4 about-body">
         <p>
-        My name is Jaron Kenyon. I am a full stack web development student, out of Phoenix, AZ. I am currently earning a certificate at the University of Arizona. I am eager to expand my knowledge and earn opportunities to practice my skills.
+        My name is Jaron Kenyon. I am a full stack web development student, out of Phoenix, AZ. I am currently earning a certificate at the University of Arizona. 
+        In my time working in the world of web development, I have worked with many different tools from HTML and CSS to creating applications that utilize the whole MERN stack.
+        I am eager to expand my knowledge and earn opportunities to practice and develop my skills.
       </p>
       </div>
     </section>
