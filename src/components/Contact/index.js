c0bb3d4764bee10f21243a16f4104e459ede80cb
Jaron <jjkenyon777@gmail.com>
2022-03-22 17:30:22 -0700

    added functioning contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,62 +1,90 @@
 import React, {useState} from 'react';
 import { validateEmail } from '../../utils/helpers';
-
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
 
 function ContactForm() {
-    const [errorMessage, setErrorMessage] = useState('');
-    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
-    const { name, email, message } = formState;
-    function handleChange(e) {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            // isValid conditional statement
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-              } else {
-                setErrorMessage('');
-              }
-              
-          }  else {
-            if (!e.target.value.length) {
-              setErrorMessage(`${e.target.name} is required.`);
-            } else {
-              setErrorMessage('');
-            }
-          }
-          
-          
-       if (!errorMessage) {setFormState({...formState, [e.target.name]: e.target.value })}
-      }
-      function handleSubmit(e) {
-          e.preventDefault();
-          console.log(formState);
-      }
+const form = useRef();
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    process.env.REACT_APP_SERVICE_ID,
+    process.env.REACT_APP_TEMPLATE_ID,
+    form.current,
+    process.env.REACT_APP_USER_ID)
+    .then((result) => {
+      console.log(result.text);
+      alert("Sent!");
+    }, (error) => {
+      console.log(error.text);
+      alert("FAILED...", error);
+    });
+};
+
+
       
     return (
-        <section>
-          <h1>Contact me</h1>
-          <form id="contact-form" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
-            </div>
-            <div>
-            <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}  />
-            </div>
-            {errorMessage && (
-  <div>
-    <p className="error-text">{errorMessage}</p>
+        
+    <div className="container">
+    <div className="row">
+      <div className="col align-self-center">
+        <h1 className="text-center">Contact me by Email:</h1>
+        {/* <!-- contact form --> */}
+        <form ref={form} onSubmit={sendEmail}>
+          {/* <!-- name --> */}
+          <div className="form-group">
+            <label for="name">Name</label>
+            <input
+              type="name"
+              name="name"
+              className="form-control"
+              id="name"
+              placeholder="enter your name"
+            />
+          </div>
+
+          {/* <!-- email --> */}
+          <div className="form-group">
+            <label for="email">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="email"
+              placeholder="enter your email"
+            />
+          </div>
+
+          {/* <!-- subject --> */}
+          <div className="form-group">
+            <label for="subject">Subject</label>
+            <input
+              type="text"
+              name="subject"
+              className="form-control"
+              id="subject"
+              placeholder="enter email subject"
+            />
+          </div>
+
+          <div className="form-group">
+            <label for="email_body">Message</label>
+            <textarea
+              className="form-control"
+              id="email_body"
+              rows="5"
+            ></textarea>
+          </div>
+
+          <button type="submit" className="contactBtn">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
-)}
-            <button className="" type="submit">Submit</button>
-          </form>
-        </section>
       )
     
 }
